9a9e91d360dca95365d7f14d773ed13d6ddd0738
unknown <nknaveenkumar760@gmail.com>
2019-03-11 18:08:58 +0530

    commit

--- pythontutorial/urls.py
+++ pythontutorial/urls.py
@@ -5,7 +5,7 @@ from django.urls import path
 urlpatterns = [
           path('ckeditor/', include('ckeditor_uploader.urls')),
           path('python', views.pythonlang, name='python'),
-          path('html', views.htmllang, name='html'),
+          path('html_language', views.htmllang, name='html_language'),
           url(r'^python_overview/(?P<page>[0-9]+)/$', views.python_tutorial, name='python_overview'),
           url(r'^html_tutorial/(?P<page>[0-9]+)/$', views.html_tutorial, name='html_tutorial'),
           path('videofile', views.showvideos, name='videofile')
